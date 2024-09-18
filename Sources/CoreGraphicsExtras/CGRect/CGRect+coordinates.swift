7378a73de6c6c6809e451c79de8b4b8c00bf78e0
whutao <whutao.dev@gmail.com>
2024-09-18 19:11:15 +0300

    Updates.

---
+++ Sources/CoreGraphicsExtras/CGRect/CGRect+coordinates.swift
@@ -0,0 +1,20 @@
+#if canImport(CoreGraphics)
+import CoreGraphics
+
+extension CGRect {
+    
+    /// Center x-coordinate of the rectangle.
+    @inlinable
+    public var centerX: CGFloat {
+        get { center.x }
+        set { center.x = newValue }
+    }
+    
+    /// Center y-coordinate of the rectangle.
+    @inlinable
+    public var centerY: CGFloat {
+        get { center.y }
+        set { center.y = newValue }
+    }
+}
+#endif
