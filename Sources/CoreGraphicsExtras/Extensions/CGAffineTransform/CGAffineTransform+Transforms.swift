cfc6ffb677b9ae4b8849d35def9de0370294608a
whutao <whutao.dev@gmail.com>
2025-05-08 21:55:56 +0300

    Fix code smells.

--- Sources/CoreGraphicsExtras/Extensions/CGAffineTransform/CGAffineTransform+Transforms.swift
+++ Sources/CoreGraphicsExtras/Extensions/CGAffineTransform/CGAffineTransform+Transforms.swift
@@ -2,37 +2,40 @@ import CoreGraphics
 
 extension CGAffineTransform {
     
-    /// Returns an affine transformation matrix constructed from translation values you provide.
+    /// Returns an affine transformation matrix constructed from a translation vector.
+    @inlinable
+    
+    /// Returns an affine transformation matrix constructed from translation values.
     @inlinable
     public static func translate(x: CGFloat, y: CGFloat) -> CGAffineTransform {
         return CGAffineTransform(translationX: x, y: y)
     }
     
-    /// Returns an affine transformation matrix constructed from translation values you provide.
+    /// Returns an affine transformation matrix constructed from translation values.
     @inlinable
     public static func translate(x: CGFloat) -> CGAffineTransform {
         return translate(x: x, y: 0)
     }
     
-    /// Returns an affine transformation matrix constructed from translation values you provide.
+    /// Returns an affine transformation matrix constructed from translation values.
     @inlinable
     public static func translate(y: CGFloat) -> CGAffineTransform {
         return translate(x: 0, y: y)
     }
     
-    /// Returns an affine transformation matrix constructed from a rotation value you provide.
+    /// Returns an affine transformation matrix constructed from a rotation value.
     @inlinable
     public static func rotate(angle: CGFloat) -> CGAffineTransform {
         return CGAffineTransform(rotationAngle: angle)
     }
     
-    /// Returns an affine transformation matrix constructed from scaling values you provide.
+    /// Returns an affine transformation matrix constructed from scaling values.
     @inlinable
     public static func scale(x: CGFloat, y: CGFloat) -> CGAffineTransform {
         return CGAffineTransform(scaleX: x, y: y)
     }
     
-    /// Returns an affine transformation matrix constructed from scaling values you provide.
+    /// Returns an affine transformation matrix constructed from scaling values.
     @inlinable
     public static func scale(_ value: CGFloat) -> CGAffineTransform {
         return scale(x: value, y: value)
--- Sources/CoreGraphicsExtras/Extensions/CGSize/CGSize+SpecialValues.swift
+++ Sources/CoreGraphicsExtras/Extensions/CGSize/CGSize+SpecialValues.swift
@@ -3,16 +3,16 @@ import CoreGraphics
 extension CGSize {
     
     /// Unit square.
-    public static let unit: CGSize = CGSize(width: 1, height: 1)
+    public static let unit = CGSize(width: 1, height: 1)
     
     /// Boundless area.
-    public static let infinity: CGSize = CGSize(
+    public static let infinity = CGSize(
         width: CGFloat.infinity,
         height: CGFloat.infinity
     )
     
     /// Least positive area.
-    public static let leastNonzeroMagnitude: CGSize = CGSize(
+    public static let leastNonzeroMagnitude = CGSize(
         width: CGFloat.leastNonzeroMagnitude,
         height: CGFloat.leastNonzeroMagnitude
     )
@@ -22,13 +22,13 @@ extension CGSize {
     /// This value compares less than or equal to all positive normal numbers.
     /// There may be smaller positive numbers, but they are *subnormal*,
     /// meaning that they are represented with less precision than normal numbers.
-    public static let leastNormalMagnitude: CGSize = CGSize(
+    public static let leastNormalMagnitude = CGSize(
         width: CGFloat.leastNormalMagnitude,
         height: CGFloat.leastNormalMagnitude
     )
     
     /// Greatest finite area.
-    public static let greatestFiniteMagnitude: CGSize = CGSize(
+    public static let greatestFiniteMagnitude = CGSize(
         width: CGFloat.greatestFiniteMagnitude,
         height: CGFloat.greatestFiniteMagnitude
     )
